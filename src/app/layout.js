bf2fdd57a8226ae0ad218bf5e0c3bac6a61ad376
hudyu17 <hengjeung.yuen@gmail.com>
2023-11-18 01:07:12 -0500

    scripts

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,8 +13,6 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         {children}
-        <script src="../path/to/flowbite/dist/flowbite.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
       </body>
     </html>
   )
